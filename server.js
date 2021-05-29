88acfffeb146c1f86de377fe678ad081f79e1e64
Cameron Chardukian <camchardukian@gmail.com>
2021-05-29 17:45:41 +0700

    feature/connect-database

--- server.js
+++ server.js
@@ -1,52 +1,66 @@
-require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const expect = require("chai").expect;
+const cors = require("cors");
 
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const apiRoutes         = require('./routes/api.js');
-const runner            = require('./test-runner');
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const apiRoutes = require("./routes/api.js");
+const runner = require("./test-runner");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("Cam's app has connected to MongoDB");
+});
+db.on("error", err => {
+  console.log(err);
+});
+
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route("/").get(function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
 // User routes
 apiRoutes(app);
-    
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+  res
+    .status(404)
+    .type("text")
+    .send("Not Found");
 });
 
 //Start our server and tests!
-const PORT = process.env.PORT || 3000
-app.listen(PORT, function () {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
   console.log("Listening on port " + PORT);
   // process.env.NODE_ENV='test'
-  if (process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
+    setTimeout(function() {
       try {
         runner.run();
       } catch (error) {
-        console.log('Tests are not valid:');
+        console.log("Tests are not valid:");
         console.error(error);
       }
     }, 1500);
